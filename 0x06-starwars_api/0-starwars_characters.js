2a02e13be86c9ae95d6a9addcb5e2937ec6ee62b
Chad96 <chadrackndalamba@gmail.com>
2024-08-11 12:24:44 +0200

    update

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,31 +1,17 @@
 #!/usr/bin/node
-
 const request = require('request');
-
 const movieId = process.argv[2];
-const movieEndpoint = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
 
-function sendRequest (characterList, index) {
-  if (characterList.length === index) {
-    return;
-  }
-
-  request(characterList[index], (error, response, body) => {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log(JSON.parse(body).name);
-      sendRequest(characterList, index + 1);
-    }
-  });
-}
-
-request(movieEndpoint, (error, response, body) => {
-  if (error) {
-    console.log(error);
-  } else {
-    const characterList = JSON.parse(body).characters;
-
-    sendRequest(characterList, 0);
+request(apiUrl, (error, response, body) => {
+  if (!error && response.statusCode === 200) {
+    const characters = JSON.parse(body).characters;
+    characters.forEach((character) => {
+      request(character, (err, res, charBody) => {
+        if (!err && res.statusCode === 200) {
+          console.log(JSON.parse(charBody).name);
+        }
+      });
+    });
   }
 });
